b49bc733d43d537cd5468a0a6f5a1e15cc55d11b
ityuany <519495771@qq.com>
2025-07-09 11:40:27 +0800

    重构DuplicateDependencyPlugin插件，更新钩子从CompilerFinishMake更改为CompilerAfterEmit，以优化插件的执行时机并提高代码可读性。移除未使用的finish_make函数实现，保留相关注释以供未来参考。

--- crates/spack_plugin_duplicate_dependency/src/lib.rs
+++ crates/spack_plugin_duplicate_dependency/src/lib.rs
@@ -5,7 +5,8 @@ use std::{collections::HashMap, path::PathBuf, time::Instant};
 use derive_more::Debug;
 use package_json_parser::PackageJsonParser;
 use rspack_core::{
-  ApplyContext, Compilation, CompilerFinishMake, CompilerOptions, Plugin, PluginContext,
+  ApplyContext, Compilation, CompilerAfterEmit, CompilerFinishMake, CompilerOptions, Plugin,
+  PluginContext,
 };
 use rspack_hook::{plugin, plugin_hook};
 use up_finder::UpFinder;
@@ -37,17 +38,24 @@ impl Plugin for DuplicateDependencyPlugin {
     ctx: PluginContext<&mut ApplyContext>,
     _options: &CompilerOptions,
   ) -> rspack_error::Result<()> {
+    // ctx
+    //   .context
+    //   .compiler_hooks
+    //   .finish_make
+    //   .tap(finish_make::new(self));
+
     ctx
       .context
       .compiler_hooks
-      .finish_make
-      .tap(finish_make::new(self));
+      .after_emit
+      .tap(after_emit::new(self));
+
     Ok(())
   }
 }
 
-#[plugin_hook(CompilerFinishMake for DuplicateDependencyPlugin)]
-async fn finish_make(&self, compilation: &mut Compilation) -> rspack_error::Result<()> {
+#[plugin_hook(CompilerAfterEmit for DuplicateDependencyPlugin)]
+async fn after_emit(&self, compilation: &mut Compilation) -> rspack_error::Result<()> {
   let start_time = Instant::now();
 
   let module_graph = compilation.get_module_graph();
@@ -99,3 +107,57 @@ async fn finish_make(&self, compilation: &mut Compilation) -> rspack_error::Resu
 
   Ok(())
 }
+
+// #[plugin_hook(CompilerFinishMake for DuplicateDependencyPlugin)]
+// async fn finish_make(&self, compilation: &mut Compilation) -> rspack_error::Result<()> {
+//   let start_time = Instant::now();
+
+//   let module_graph = compilation.get_module_graph();
+
+//   let mut cache = HashMap::new();
+
+//   for (_identifier, module) in module_graph.modules().iter() {
+//     if module.module_type().is_js_like()
+//       && let readable_name = module.readable_identifier(&compilation.options.context)
+//       && let Some(dir) = PathBuf::from(readable_name.to_string()).parent()
+//     {
+//       let up_finder = UpFinder::builder().cwd(dir).build();
+//       let paths = up_finder.find_up("package.json");
+
+//       if let Some(library) = paths
+//         .iter()
+//         .filter(|path| {
+//           let path_str = path.to_string_lossy().to_string();
+//           let cached = !cache.contains_key(path_str.as_str());
+//           let is_node_modules = path_str.contains("node_modules");
+//           cached && is_node_modules
+//         })
+//         .find_map(|path| {
+//           if let Ok(package_json) = PackageJsonParser::parse(path)
+//             && let Some(name) = package_json.name
+//             && let Some(version) = package_json.version
+//             && let Some(path) = package_json.__raw_path
+//           {
+//             return Some(Library::new(path.clone(), name.0, version.0));
+//           }
+//           return None;
+//         })
+//       {
+//         cache.insert(library.dir.clone(), library);
+//       }
+//     }
+//   }
+
+//   let end_time = Instant::now();
+//   let duration = end_time.duration_since(start_time);
+
+//   let duration = duration.as_millis() as f64;
+
+//   let response = DuplicateDependencyPluginResp::new(cache.values().cloned().collect(), duration);
+
+//   if let Some(on_detected) = &self.options.on_detected {
+//     on_detected(response).await?;
+//   }
+
+//   Ok(())
+// }
