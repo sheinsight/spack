6d7bf84459dbf602eefb87ec1dedc186dd10e884
ityuany <519495771@qq.com>
2025-09-25 19:14:31 +0800

    更新 response.json 中的时间戳，新增 write_runtime_by_alias 方法以增强样式加载器的别名处理逻辑，提升代码可读性和一致性。

--- crates/spack_loader_style/src/plugin.rs
+++ crates/spack_loader_style/src/plugin.rs
@@ -1,16 +1,19 @@
 use std::{ops::Not, sync::Arc};
 
 use rspack_core::{
-  ApplyContext, BoxLoader, Context, ModuleRuleUseLoader, NormalModuleFactoryResolveLoader, Plugin,
-  Resolver,
+  Alias, ApplyContext, BoxLoader, Context, ModuleRuleUseLoader, NormalModuleFactoryResolveLoader,
+  Plugin, Resolver,
 };
 use rspack_error::Result;
 use rspack_hook::{plugin, plugin_hook};
 use rspack_paths::Utf8PathBuf;
+use rspack_resolver::AliasValue;
 
 use crate::loader::{STYLE_LOADER_IDENTIFIER, StyleLoader, StyleLoaderOpts};
 
 pub const STYLE_LOADER_PLUGIN_IDENTIFIER: &str = "Spack.StyleLoaderPlugin";
+
+const ALIAS_NAME: &str = "@@";
 
 #[plugin]
 #[derive(Debug)]
@@ -87,6 +90,38 @@ impl StyleLoaderPlugin {
 
     Ok(())
   }
+
+  pub fn write_runtime_by_alias(&self, alias_config: &Option<Alias>) -> Result<()> {
+    let err_msg = "StyleLoaderPlugin requires the alias '@@' to be configured.”";
+
+    let Some(alias) = alias_config else {
+      return Err(rspack_error::error!(err_msg.to_string()));
+    };
+
+    let tuple_aliases = match alias {
+      Alias::MergeAlias(items) => items,
+      Alias::OverwriteToNoAlias => {
+        return Err(rspack_error::error!(err_msg.to_string()));
+      }
+    };
+
+    let Some((_, aliases)) = tuple_aliases.iter().find(|(k, _v)| (k == ALIAS_NAME)) else {
+      return Err(rspack_error::error!(err_msg.to_string()));
+    };
+
+    if aliases.is_empty() {
+      return Err(rspack_error::error!(err_msg.to_string()));
+    }
+
+    for alias in aliases {
+      if let AliasValue::Path(path) = alias {
+        let path = Utf8PathBuf::from(path.to_string()).join(&self.options.output);
+        Self::write_runtime(&path)?;
+      }
+    }
+
+    Ok(())
+  }
 }
 
 impl Plugin for StyleLoaderPlugin {
@@ -95,41 +130,7 @@ impl Plugin for StyleLoaderPlugin {
   }
 
   fn apply(&self, ctx: &mut ApplyContext) -> rspack_error::Result<()> {
-    if let Some(alias) = &ctx.compiler_options.resolve.alias {
-      let value = match alias {
-        rspack_core::Alias::OverwriteToNoAlias => None,
-        rspack_core::Alias::MergeAlias(items) => {
-          let alias_values = items
-            .iter()
-            .find_map(|(k, v)| if k == "@@" { Some(v) } else { None });
-
-          if let Some(alias) = alias_values {
-            alias.get(0)
-          } else {
-            None
-          }
-        }
-      };
-
-      if let Some(value) = value {
-        match value {
-          rspack_resolver::AliasValue::Path(path) => {
-            let path = path.to_string();
-            let path = Utf8PathBuf::from(path).join(&self.options.output);
-            Self::write_runtime(&path)?;
-          }
-          rspack_resolver::AliasValue::Ignore => {
-            return Err(rspack_error::error!(
-              "StyleLoaderPlugin requires alias to be configured with '@@'"
-            ));
-          }
-        }
-      } else {
-        return Err(rspack_error::error!(
-          "StyleLoaderPlugin requires alias to be configured with '@@'"
-        ));
-      }
-    }
+    self.write_runtime_by_alias(&ctx.compiler_options.resolve.alias)?;
 
     ctx
       .normal_module_factory_hooks
